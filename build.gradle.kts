439890501fad9f75e697ffa57746630edc4e37bb
Lorenzo Debertolis <lorenzo.debertolis@studio.unibo.it>
2023-03-21 11:39:49 +0100

    added basic gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    application
+    id("org.danilopianini.gradle-java-qa") version "1.4.0"
+    //id(org.openjfx.javafxplugin") version "0.0.13"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Use JUnit Jupiter API for testing.
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+
+    // Use JUnit Jupiter Engine for testing.
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
+
+//javafx {
+//    version = "19"
+//    modules = [ 'javafx.controls' ]
+//}
+
+application {
+    mainClass.set("it.tbt.TurnBasedTunnels")
+}
+
