506cccf19802fc36e635cbfabdc7842641b6bbe2
Federico Tamburini <federico.tamburini@studio.unibo.it>
2023-06-20 16:54:02 +0200

    fixed checkstyles

--- src/test/java/it/tbt/model/fight/impl/FightModelImplTest.java
+++ src/test/java/it/tbt/model/fight/impl/FightModelImplTest.java
@@ -20,9 +20,21 @@ import it.tbt.model.entities.characters.Enemy;
 import it.tbt.model.party.IParty;
 import it.tbt.model.party.Party;
 
+/**
+ * This class is used to test the implementation of the {@link FightModelImpl}
+ * class.
+ */
 public final class FightModelImplTest {
 
     private FightModelImpl fightModel;
+
+    private static final int BASE_HEALTH = 10;
+    private static final int BASE_ATTACK = 10;
+    private static final int ALLY1_SPEED = 100;
+    private static final int ALLY2_SPEED = 110;
+    private static final int ALLY3_SPEED = 120;
+    private static final int ALLY4_SPEED = 130;
+    private static final int DEFAULT_POSITION_AND_SIZE = 75;
 
     @SuppressWarnings("MagicNumber")
     @BeforeEach
@@ -30,21 +42,26 @@ public final class FightModelImplTest {
         int averageEnemyStat = 10;
         final Map<Item, Double> drops = new HashMap<>();
         final List<Ally> allies = new ArrayList<>();
-        allies.add(new Ally("Roberto", 1, 10, 100));
-        allies.add(new Ally("Roberto", 9, 10, 110));
-        allies.add(new Ally("Gianfranco", 5, 10, 120));
-        allies.add(new Ally("Roberto", 10, 10, 130));
+        allies.add(new Ally("Turgico", BASE_HEALTH, BASE_ATTACK, ALLY1_SPEED));
+        allies.add(new Ally("Marco", BASE_HEALTH, BASE_ATTACK, ALLY2_SPEED));
+        allies.add(new Ally("Gianfranco", BASE_HEALTH, BASE_ATTACK, ALLY3_SPEED));
+        allies.add(new Ally("Roberto", BASE_HEALTH, BASE_ATTACK, ALLY4_SPEED));
         fightModel = new FightModelImpl(averageEnemyStat, drops);
-        final IParty party = new Party("Party", 75, 75, 75, 75, allies);
+        final IParty party = new Party("Party", DEFAULT_POSITION_AND_SIZE, DEFAULT_POSITION_AND_SIZE,
+                DEFAULT_POSITION_AND_SIZE, DEFAULT_POSITION_AND_SIZE, allies);
 
         fightModel.initializeParty(party);
     }
 
+    /**
+     * Tests the initialization of the party in the fight model.
+     */
     @SuppressWarnings("MagicNumber")
     @Test
     public void testInitializeParty() {
         // Create a party
-        Party party = new Party("Party", 75, 75, 75, 75);
+        Party party = new Party("Party", DEFAULT_POSITION_AND_SIZE, DEFAULT_POSITION_AND_SIZE,
+                DEFAULT_POSITION_AND_SIZE, DEFAULT_POSITION_AND_SIZE);
         Ally ally1 = new Ally("Ally 1", 20, 5, 8);
         Ally ally2 = new Ally("Ally 2", 18, 7, 6);
         party.addMember(ally1);
@@ -60,21 +77,33 @@ public final class FightModelImplTest {
         Assertions.assertTrue(allies.contains(ally2));
     }
 
+    /**
+     * Tests the retrieval of all allies from the fight model.
+     */
     @Test
     public void testGetAllies() {
         Assertions.assertNotNull(fightModel.getAllies());
     }
 
+    /**
+     * Tests the retrieval of all enemies from the fight model.
+     */
     @Test
     public void testGetEnemies() {
         Assertions.assertNotNull(fightModel.getEnemies());
     }
 
+    /**
+     * Tests the retrieval of the current ally from the fight model.
+     */
     @Test
     public void testGetCurrentAlly() {
         Assertions.assertNotNull(fightModel.getCurrentAlly());
     }
 
+    /**
+     * Tests the selection of an action in the fight model.
+     */
     @Test
     public void testSelectAction() {
         // Select to use a skill
@@ -96,6 +125,9 @@ public final class FightModelImplTest {
         assertTrue(fightModel.isUsingAntidote());
     }
 
+    /**
+     * Tests the advancement of the turn in the fight model.
+     */
     @Test
     void testAdvanceTurn() {
         Ally ally1 = fightModel.getCurrentAlly();
@@ -105,6 +137,9 @@ public final class FightModelImplTest {
         assertEquals(ally2, fightModel.getCurrentAlly());
     }
 
+    /**
+     * Tests the performance of the selected action in the fight model.
+     */
     @Test
     void testPerformSelectedAction() {
         // Simulate selecting and performing a skill action
@@ -123,6 +158,9 @@ public final class FightModelImplTest {
         assertTrue(sumHPEnemyAfter < sumHPEnemy);
     }
 
+    /**
+     * Tests the selection of the next target in the fight model.
+     */
     @SuppressWarnings("MagicNumber")
     @Test
     void testSelectNextTarget() {
@@ -136,6 +174,9 @@ public final class FightModelImplTest {
         assertTrue(updatedTargetIndex == initialTargetIndex + 1 || updatedTargetIndex == 3);
     }
 
+    /**
+     * Tests the selection of the previous target in the fight model.
+     */
     @SuppressWarnings("MagicNumber")
     @Test
     void testSelectPreviousTarget() {
